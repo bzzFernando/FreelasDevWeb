7ab3fe3b0f4389a27dd1f6fdeb756b8b5d30cdf2
Fernando Rafael <38268356+bzzFernando@users.noreply.github.com>
2018-04-25 16:06:32 -0300

    Adicionar Rodapé
    Adicionar Rodapé. Vocês que entendem mais de Styel, mandem as fontes que vão usar para colocar no Rodapé!

---
+++ Rodape.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rafa-
+ * Date: 25/04/2018
+ * Time: 16:00
+ */
+?>
+
+<div style="background-color: #1d2124">
+    <center>
+    <footer>
+        <small>
+            <p style="color: white; font-family: 'Courier New'"> &copy Copyright 2018 - Freelas - F.R </p>
+        </small>
+    </footer>
+    </center>
+</div>
+</body>
+</html>
