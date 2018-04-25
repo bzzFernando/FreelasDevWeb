d3f2ba269fe60dafdd999f2047da070716a6d0d0
Fernando Rafael <38268356+bzzFernando@users.noreply.github.com>
2018-04-25 19:59:55 -0300

    Edição Nav Bar
    Feita por Manoel/Fernando 20h -  25/04/2018

--- Cabecalho.php
+++ Cabecalho.php
@@ -18,41 +18,86 @@
     <meta name = "application-name"  content= "Freelas Dev WebSite">
 
     <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+
 </head>
 
 <body>
 <header>
+
     <nav class="navbar navbar-dark bg-dark">
         <a class="navbar-brand" href="home.php">Freelas Web Dev</a>
         <div class="navbar-header">
+            <form class="form-check-inline">
+                <input class="form-control" type="search" placeholder="Search">
+                <button type="submit" class="btn btn-outline-info mx-2">Buscar</button>
+            </form>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navFreelas" aria-controls="navFreelas" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         </div>
 
 
-        <div class="collapse navbar-collapse" id="navFreelas">
-            <ul class="navbar-nav mr-auto">
+        <div class="collapse navbar-collapse navbar-expand-md" id="navFreelas">
+            <div class="container-fluid mt-2">
+                <div class="row">
+                    <ul class="navbar-nav mr-auto col-lg-9 float-left">
 
-                <li class="nav-item">
-                    <a class="nav-link" href="Sobre.php">Sobre</a>
-                </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="Sobre.php">Sobre</a>
+                        </li>
 
-                <li class="nav-item">
-                    <a class="nav-link" href="Trabalhos.php">Trabalhos</a>
-                </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="Trabalhos.php">Trabalhos</a>
+                        </li>
 
-                <li class="nav-item">
-                    <a class="nav-link" href="Suporte.php">Suporte</a>
-                </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="Suporte.php">Suporte </a>
+                        </li>
 
-                <li class="nav-item">
-                    <a class="nav-link" href="Contato.php">Contato</a>
-                </li>
-            </ul>
+                        <li class="nav-item">
+                            <a class="nav-link" href="Contato.php">Contato </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+
         </div>
     </nav>
 
+    <div class="container-fluid mt-2 text-danger">
+        <div class="row">
+            <div class="col-md-6">
+                <h4 class="text-center">Freelas WEB DEV</h4>
+            </div>
+            <div class="col-sm-12 col-md-6 ">
+                <ul class="nav mx-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="Sobre.php">Sobre</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="Trabalhos.php">Trabalhos</a>
+                    </li>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+                    <li class="nav-item">
+                        <a class="nav-link" href="Suporte.php">Suporte </a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="Contato.php">Contato </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- SCRIPTS BOOTSTRAP !-->
+
+<!--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>  !-->
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+
 <script src="bootstrap/js/bootstrap.min.js  "> </script>
