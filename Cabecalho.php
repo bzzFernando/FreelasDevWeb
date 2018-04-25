a39949fbcdc8955e03b070b9ad66fc2700a72593
Fernando Rafael <38268356+bzzFernando@users.noreply.github.com>
2018-04-25 15:57:14 -0300

    Cabeçalho com External Content
    Coloquei aquele botão pra abrir o menu.
    Achei que ficou grande, mas que a primeira visão ficou mais "clean".
    Quero saber de vocês se acham a ideia boa ou vamos manter o "padrão" menu de sempre!

---
+++ Cabecalho.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rafa-
+ * Date: 25/04/2018
+ * Time: 15:11
+ */
+?>
+
+<!DOCTYPE html>
+<html lang = "pt-BR">
+<head>
+    <meta charset="utf-8">
+    <meta name = " Author"  content="FreelasDev">
+    <meta name = "Generator" content= "PHP Storm created by JETBRAINS">
+    <meta name= "Keywords" content= "Desenvolvimento WebSite Palmas Social Media">
+    <meta name= "Description" content= "Site empresarial Freelas Dev, especializada em Desenvolvimento Web e Midia Social">
+    <meta name = "application-name"  content= "Freelas Dev WebSite">
+
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+</head>
+
+<body>
+<header>
+    <nav class="navbar navbar-dark bg-dark">
+        <a class="navbar-brand" href="home.php">Freelas Web Dev</a>
+        <div class="navbar-header">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navFreelas" aria-controls="navFreelas" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        </div>
+
+
+        <div class="collapse navbar-collapse" id="navFreelas">
+            <ul class="navbar-nav mr-auto">
+
+                <li class="nav-item">
+                    <a class="nav-link" href="Sobre.php">Sobre</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="Trabalhos.php">Trabalhos</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="Suporte.php">Suporte</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="Contato.php">Contato</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js  "> </script>
